1347d83a34b7c43c36e2897e5c83223717524a0c
Rahul Salvi <rsalvi006@gmail.com>
2024-08-17 22:54:22 -0700

    don't hardcode dns1

--- start.sh
+++ start.sh
@@ -6,6 +6,23 @@ go install tailscale.com/cmd/get-authkey@latest
 go install github.com/charmbracelet/gum@latest
 export PATH="${GOPATH}/bin:${PATH}"
 
+if [ -f .env ]; then
+	source .env
+else
+	echo "Choose hostname for new dns server"
+	NAME=$(gum input --placeholder "dnsX")
+	echo "NAME=${NAME}" >.env
+	echo "Wrote variables to .env"
+fi
+
+echo "Managing ${NAME}"
+
+if tailscale ip "${NAME}" >/dev/null 2>&1; then
+	echo "${NAME} seems to already exist. You should remove it before continuing"
+	echo "https://login.tailscale.com/admin/machines"
+	exit 1
+fi
+
 echo "Enter your tailscale API client ID"
 echo "https://login.tailscale.com/admin/settings/oauth"
 TS_API_CLIENT_ID=$(gum input --password)
@@ -20,16 +37,13 @@ echo "Enter initial admin password for technitium"
 TECHNITIUM_PASSWORD=$(gum input --password)
 export TECHNITIUM_PASSWORD
 
-if tailscale ip technitium >/dev/null 2>&1; then
-	echo "technitium seems to already exist. You should remove it before continuing"
-	echo "https://login.tailscale.com/admin/machines"
-	exit 1
-fi
-
 echo "Generating tailscale auth keys"
 TS_AUTHKEY=$(get-authkey -ephemeral -preauth -tags tag:nameserver)
 export TS_AUTHKEY
 
 gum spin --title "Starting up" -- docker compose up -d --build
+
+echo "Remember to update the tailnet DNS servers"
+echo "https://login.tailscale.com/admin/dns"
 
 echo "Done!"
