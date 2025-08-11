d749b671b1ac1585a77b89a3c02f9a2a187ce02c
Rahul Salvi <rsalvi006@gmail.com>
2025-08-10 22:05:46 -0700

    update docker-compose-snippets and add pre-deploy script

---
+++ pre-deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+TS_TAGS=tag:webserver,tag:nameserver,tag:recursive
+
+# return code
+RC=0
+
+git submodule update --init --recursive
+
+if [ ! -e .env ]; then
+    touch .env
+fi
+chmod -f 600 .env
+
+if ! grep -q "ID" .env; then
+    echo "ERROR: no ID variable"
+    RC=1
+fi
+
+if ! grep -q "TS_AUTHKEY" .env; then
+    if [ -z "$TS_API_CLIENT_ID" ]; then
+        echo "ERROR: no tailscale oauth credentials provided"
+        RC=1
+    else
+        echo "Adding TS_AUTHKEY to .env"
+        echo "TS_AUTHKEY=$(get-authkey -tags "$TS_TAGS" -ephemeral -preauth)" >>.env
+    fi
+fi
+
+exit $RC
